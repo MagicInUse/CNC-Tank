c9465cfcfa8a4c4a524c861531cd9889a9cacd7b
MagicInUse <jacob.norr@live.com>
2025-02-01 13:20:52 -0700

    spindle integration

--- client/src/assets/SVGs.jsx
+++ client/src/assets/SVGs.jsx
@@ -212,3 +212,20 @@ export function WifiSVG(props) {
     </svg>
   )
 }
+
+export function SpindleSVG(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="1 10 15 12"
+      width="2em"
+      height="2em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M13.5 17v2L12 22l-1.5-3zm1-10.7l-1 .7V6h-3v3l-1 .7v1l5-3.3zm0 4l-1 .7V9l-3 2v2l-1 .7v1l5-3.3zm0 4l-1 .7v-2l-3 2v2l-1 .7v1l5-3.3zM8 2S7 2 7 3l3 2h4l3-2s0-1-1-1z"
+      ></path>
+    </svg>
+  )
+}
