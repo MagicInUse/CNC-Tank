36cdcfd9fd89d1d617c3a4390836a7703f6c6e1e
MagicInUse <jacob.norr@live.com>
2025-02-01 02:06:14 -0700

    merge conflict

--- _ESP32/machine.cpp
+++ _ESP32/machine.cpp
@@ -58,6 +58,17 @@ void handleTestControl() {
     Serial.println("Direction: " + direction);
     Serial.println("Speed: " + String(speed));
     Serial.println("Step: " + String(step));
+
+    if (axis == 'z' && direction == 'up') {
+        zMove(direction, speed, step);
+    }
+    else if (axis == 'z' && direction == 'down') {
+        zMove(direction, speed, step);
+    }
+    else {
+        server.send(400, "application/json", "{\"error\": \"Invalid axis or direction\"}");
+        return;
+    }
     
     StaticJsonDocument<200> response;
     response["status"] = "success";
@@ -74,7 +85,7 @@ void setup() {
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
     pinMode(enb, OUTPUT);
-    
+
     //Disable stepper at start up.
     digitalWrite(enb, HIGH);
 
