4284912c93513160a29feb3e062656dcee2f721a
JarenElectron <jarenhudson@gmail.com>
2025-03-02 19:09:29 -0700

    Updating group.

--- server/src/utils/Planner.js
+++ server/src/utils/Planner.js
@@ -4,3 +4,9 @@
 
 //GRBL dependant variables (All outputs will be in absolute positioning - or # of steps from work Origin. With bounds being stock Origin)
 
+//Console dependant functions/returns for offload to ESP32 hardware.
+
+//File dependant functions/returns for offload to ESP32 and Visual Feedback on client side.
+
+//ESTOP dependant functions/returns for offload to ESP32 hardware. (I.E when someone wishes to stop an action. The robots work area is massive and remote kill of que may be necessary.)
+//Although the planner needs to know where the EStop occured in space so that a recovery can be attempted - as large work perices can be expensive to replace.
